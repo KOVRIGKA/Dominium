b4ed8ec166febc1bf58fe3b66f1dc2538ebf43af
Kovrigina Anastasia <anastasiia.kovrighina@mail.ru>
2019-06-11 12:56:32 +0300

    validity 1

--- js/main.js
+++ js/main.js
@@ -50,18 +50,44 @@ function sendFirstOrder() {
 	  sendFirstForm.addEventListener('click', function (event) {
 		event.preventDefault();
 
-		let url = firstForm.getAttribute('action');
-		let method = firstForm.getAttribute('method');
-		let formData = new FormData(firstForm);
+		if (validateForm(firstForm)){
+			let url = firstForm.getAttribute('action');
+			let method = firstForm.getAttribute('method');
+			let formData = new FormData(firstForm);
+	
+			formData.append('name', firstForm.elements.name.value);
+			formData.append('phone', firstForm.elements.phone.value);
+			formData.append('to', 'info@dominium.ru');
+	
+			const xhr = new XMLHttpRequest();
+			xhr.responseType = 'json';
+			xhr.open(method, url);
+			xhr.send(formData);
+		}
+		
+		function validateForm(form){
+			let valid = true;
 
-		formData.append('name', firstForm.elements.name.value);
-		formData.append('phone', firstForm.elements.phone.value);
-		formData.append('to', 'info@dominium.ru');
+			if(!validateField(form.elements.name)){
+				valid = false;
+			}
 
-		const xhr = new XMLHttpRequest();
-		xhr.responseType = 'json';
-		xhr.open(method, url);
-		xhr.send(formData);
+			if(!validateField(form.elements.phone)){
+				valid = false;
+			}
+
+			return valid;
+		}
+
+		function validateField(field){
+			if (!field.checkValidity()){
+				field.nextElementSibling.textContent = field.validationMessage;
+				return false;
+			} else {
+				field.nextElementSibling.textContent = '';
+				return true;
+			}
+		}
 	});
 };
 sendFirstOrder();
@@ -70,27 +96,77 @@ function sendSecondOrder() {
 	const secondForm = document.querySelector('#secondForm');
     const sendSecondForm = document.querySelector('#sendSecondForm');
 
-	  sendSecondForm.addEventListener('click', function (event) {
+	sendSecondForm.addEventListener('click', function (event) {
 		event.preventDefault();
 
-		let url = secondForm.getAttribute('action');
-		let method = secondForm.getAttribute('method');
-		let formData = new FormData(secondForm);
-    
-    formData.append('housingType', secondForm.elements.housingType.value);
-    formData.append('roomNumber', secondForm.elements.roomNumder.value);
-		formData.append('metro', secondForm.elements.metro.value);
-    formData.append('floorNumber', secondForm.elements.floorNumber.value);
-		formData.append('floor', secondForm.elements.floor.value);
-		formData.append('name', secondForm.elements.name.value);
-		formData.append('mail', secondForm.elements.mail.value);
-		formData.append('phone', secondForm.elements.phone.value);    
-		formData.append('to', 'info@dominium.ru');
+		if (validateForm(secondForm)){
+			let url = secondForm.getAttribute('action');
+			let method = secondForm.getAttribute('method');
+			let formData = new FormData(secondForm);
+		
+			formData.append('housingType', secondForm.elements.housingType.value);
+			formData.append('roomNumber', secondForm.elements.roomNumder.value);
+			formData.append('metro', secondForm.elements.metro.value);
+			formData.append('floorNumber', secondForm.elements.floorNumber.value);
+			formData.append('floor', secondForm.elements.floor.value);
+			formData.append('name', secondForm.elements.name.value);
+			formData.append('mail', secondForm.elements.mail.value);
+			formData.append('phone', secondForm.elements.phone.value);    
+			formData.append('to', 'info@dominium.ru');
+	
+			const xhr = new XMLHttpRequest();
+			xhr.responseType = 'json';
+			xhr.open(method, url);
+			xhr.send(formData);
+		}
 
-		const xhr = new XMLHttpRequest();
-		xhr.responseType = 'json';
-		xhr.open(method, url);
-		xhr.send(formData);
+		function validateForm(form){
+			let valid = true;
+
+			if(!validateField(form.elements.housingType)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.roomNumder)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.metro)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.floorNumber)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.floor)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.name)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.mail)){
+				valid = false;
+			}
+
+			if(!validateField(form.elements.phone)){
+				valid = false;
+			}
+
+			return valid;
+		}
+
+		function validateField(field){
+			if (!field.checkValidity()){
+				field.nextElementSibling.textContent = field.validationMessage;
+				return false;
+			} else {
+				field.nextElementSibling.textContent = '';
+				return true;
+			}
+		}
 	});
 };
 sendSecondOrder();
